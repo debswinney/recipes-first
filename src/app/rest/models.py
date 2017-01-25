adbd05e9ee5e3ac30d652e74e521075a38f0698f
debswinney <deborah.swinney@googlemail.com>
2017-01-25 23:34:51 +0000

    Added in foreign keys to model and serializers.

--- src/app/rest/models.py
+++ src/app/rest/models.py
@@ -2,19 +2,27 @@ from django.db import models
 
 # Create your models here.
 class Meal(models.Model):
-    created = models.DateTimeField(auto_now_add=True)
     name=models.CharField(max_length=100, blank=False)
     timeFrom=models.TimeField()
     timeTo=models.TimeField()
+    created = models.DateTimeField(auto_now_add=True)
+    day = models.ForeignKey('rest.ScheduleDay', related_name='meals', on_delete=models.CASCADE)
 
 class Activity(models.Model):
-    created = models.DateTimeField(auto_now_add=True)
     name=models.CharField(max_length=100, blank=False)
     busyTimeFrom=models.TimeField()
     activityTimeFrom=models.TimeField()
     activityTimeTo=models.TimeField()
     busyTimeTo=models.TimeField()
+    created = models.DateTimeField(auto_now_add=True)
+    day = models.ForeignKey('rest.ScheduleDay', related_name='activities', on_delete=models.CASCADE)
 
 class Schedule(models.Model):
-    created = models.DateTimeField(auto_now_add=True)
     name=models.CharField(max_length=100, blank=False)
+    created = models.DateTimeField(auto_now_add=True)
+    owner=models.ForeignKey('auth.User', related_name='schedules', on_delete=models.CASCADE)
+
+class ScheduleDay(models.Model):
+    name = models.CharField(max_length=100, blank=False)
+    created = models.DateTimeField(auto_now_add=True)
+    schedule = models.ForeignKey('rest.Schedule', related_name='days', on_delete=models.CASCADE)
